9bc532b4fdc30f777ff04420f394a7ee65a7ce6c
nanzaneli23 <renanzaneli01@gmail.com>
2024-05-28 14:22:40 -0300

    Inicio do Projeto

--- src/Routes/Rotas.js
+++ src/Routes/Rotas.js
@@ -5,11 +5,9 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import { useContext } from 'react';
 import { AuthContext } from '../Context/AuthContext';
 
-import Home from '../Pages/Home';
+
 import Busca from '../Pages/Busca';
 import Login from '../Pages/Login';
-import Videos from '../Pages/Videos';
-import Perfil from '../Pages/Perfil';
 import Inserir from '../Pages/Inserir';
 
 
@@ -35,15 +33,7 @@ export default function Rotas() {
                     tabBarActiveTintColor: "white"
                 }}
             >
-                <Tab.Screen
-                    name="Home"
-                    component={Home}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="home" color={color} size={size} />
-                        ),
-                    }}
-                />
+               
                 <Tab.Screen
                     name="Busca"
                     component={Busca}
@@ -63,24 +53,7 @@ export default function Rotas() {
                         ),
                     }}
                 />
-                <Tab.Screen
-                    name="Videos"
-                    component={Videos}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="youtube-tv" color={color} size={size} />
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Perfil"
-                    component={Perfil}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="account-circle" color={color} size={size} />
-                        ),
-                    }}
-                />
+                
             </Tab.Navigator>
         </NavigationContainer>
     )
