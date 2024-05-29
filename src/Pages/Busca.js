4a3d19202be4a2c37b31b4295f006be82a348159
nanzaneli23 <renanzaneli01@gmail.com>
2024-05-29 08:32:16 -0300

    Projeto finalizado

--- src/Pages/Busca.js
+++ src/Pages/Busca.js
@@ -1,72 +1,218 @@
-import { View, Text, TextInput, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, Alert, StatusBar } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function Busca() {
-    const [usuarios, setUsuarios ] = useState( [] );
-    const [error, setError ] = useState(false);
-    const [busca, setBusca] = useState(false);
-    const [filtro, setFiltro ] = useState(false);
+    const [clients, setClients] = useState([]);
+    const [error, setError] = useState(false);
+    const [edicao, setEdicao] = useState(false);
+    const [clientId, setClientId] = useState(0);
+    const [clientNome, setNome] = useState();
+    const [clientEmail, setEmail] = useState();
+    const [clientGenero, setGenero] = useState();
+    const [deleteResposta, setResposta] = useState(false);
 
-    async function getUsuarios()
-    {
-        await fetch('https://fakestoreapi.com/users', {
+    async function getClients() {        
+        await fetch('http://10.139.75.34:5251/api/Clients/GetAllClients', {
             method: 'GET',
             headers: {
-              'content-type': 'application/json'
+                'content-type': 'application/json'
             }
-          })
-            .then( res => ( res.ok == true ) ? res.json() : false )
-            .then( json => setUsuarios( json ) )
-            .catch( err => setError( true ) )
+        })
+            .then(res => res.json())
+            .then(json => setClients(json))
+            .catch(err => setError(true))
     }
 
-    useEffect( () => {
-        getUsuarios();
-    }, [] );
 
-    useEffect( () => {
-        setFiltro( usuarios.filter( (item) => item.name.firstname == busca )[0] );
-    }, [busca] );
+    async function getClient(id) {  
+        await fetch('http://10.139.75.34:5251/api/Clients/GetClientId/' + id, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'aplication/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())           
+            .then(json => {
+                setClientId(json.clientId);
+                setNome(json.clientName);
+                setEmail(json.clientEmail);
+                setGenero(json.clientGenere);
+            });
+    }
+    async function editClient() {       
+        await fetch('http://10.139.75.34:5251/api/Clients/UpdateClients/' + clientId, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({
+                clientId: clientId,
+                clientEmail:clientEmail,
+                clientGenero: clientGenero,
+                clientName: clientNome
+            })
+        })
+        .then((response) => response.json())
+        .catch(err => console.log(err));
+        getClients();
+        setEdicao(false);
+    }
+
+    function showAlert(id,clientName) {
+      Alert.alert(
+        '', 
+        'Deseja realmente excluir esse cliente?',
+        [
+          { text: 'Sim', onPress: () => deleteClient(id, clientName)},
+          { text: 'Não', onPress: () =>('')},
+        ],
+        { cancelable: false}
+      );
+    }
+
+    async function deleteClient(id, clientName){
+      await fetch('http://10.139.75.34:5251/api/Clients/DeleteClients/' + id, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+      .then(res => res.json())
+      .then(json => setResposta(json))
+      .catch(err => setError(true))
+
+      if(deleteResposta == true)
+        {
+          Alert.alert(
+            '',
+            'Client' + clientName + 'excluido com sucesso',
+            [
+              { text: '', onPress: () => ('')},
+              { text: 'Ok', onPress: () => ('')},
+            ],
+            { cancelable: false }
+          );
+          getClients();
+        }
+        else{
+          Alert.alert(
+            '',
+            'Client' + clientName + 'não foi excluido.',
+            [
+              { text: '', onPress: () => ('')},
+              { text: 'Ok', onPress: () => ('')},
+            ],
+            { cancelable: false }
+          );
+          getClients();
+        }
+    }
+
+    useEffect(() => {
+        getClients();
+    }, []);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            getClients();
+        }, [])
+    );
 
     return (
         <View style={css.container}>
-            <View style={css.searchBox}>
-                <TextInput
-                    style={css.search}
-                    placeholder="Buscar usuarios"
-                    placeholderTextColor="white"
-                    TextInput={busca}
-                    onChangeText={(digitado) => setBusca( digitado ) }
+            <StatusBar/>
+            {edicao == false ?
+                <FlatList
+                    style={css.flat}
+                    data={clients}
+                    keyExtractor={(item) => item.ClientId}
+                    renderItem={({ item }) => (
+                        <View style={css.itemContainer}>
+                            <Text style={css.text}>
+                                {item.clientName}
+                            </Text>
+                            <TouchableOpacity style={css.btnEdit} onPress={() => { setEdicao(true); getClient(item.clientId) }}>
+                                <Text style={css.btnText}>EDITAR</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={css.btnDelete} onPress={()=> showAlert(item.clientId, item.clientName)}>
+                                <Text style={css.btnText}>EXCLUIR</Text> 
+                            </TouchableOpacity>
+                        </View>
+                    )}
                 />
-            </View>
-            { filtro && <Text style={css.text}>{filtro.name.firstname} {filtro.name.lastname}</Text> }
-            { ( !filtro && busca ) && <ActivityIndicator size="large" color="white" /> }
+                :
+                <View style={css.editar}>
+                    <TextInput
+                        inputMode="text"
+                        style={css.input}
+                        value={clientNome}
+                        onChangeText={(digitado) => setNome(digitado)}
+                        placeholderTextColor="black"
+                        placeholder='Nome'
+                    />
+                    <TextInput
+                        inputMode="email"
+                        style={css.input}
+                        value={clientEmail}
+                        onChangeText={(digitado) => setEmail(digitado)}
+                        placeholderTextColor="black"
+                        placeholder='E-mail'
+                    />
+                    <TextInput
+                        inputMode="text"
+                        secureTextEntry={true}
+                        style={css.input}
+                        value={clientGenero}
+                        onChangeText={(digitado) => setGenero(digitado)}
+                        placeholderTextColor="black"
+                        placeholder='Genero'
+                    />
+                    <TouchableOpacity style={css.btnCreate} onPress={() => editClient()}>
+                        <Text style={css.btnLoginText}>SALVAR</Text>
+                    </TouchableOpacity>
+                </View>
+            }
         </View>
     )
 }
+
 const css = StyleSheet.create({
     container: {
-        flexGrow: 1,
-        width: "100%",
-        alignItems: "center",
-        backgroundColor: "#191919",
+        width: "90%",
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        marginLeft: 20,
+        elevation: 10,
+        marginTop: 50
+    },
+    flat: {
+        // Estilos da FlatList, se necessário
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
     },
     text: {
-        color: "white"
+        // Estilos para o texto do item, se necessário
     },
-    searchBox: {
-        width: "100%",
-        height: 100,
-        justifyContent: "flex-end",
-        alignItems: "center",
+    btnEdit: {
+        backgroundColor: '#00008B',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 5,
     },
-    search: {
-        width: "96%",
-        height: 60,
-        borderWidth: 1,
-        borderColor: "white",
-        borderRadius: 8,
-        padding: 10,
-        color: "white"
-    }
-})
+    btnDelete: {
+        backgroundColor: '#FF0000',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    btnText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+});
--- src/Pages/Inserir.js
+++ src/Pages/Inserir.js
@@ -1,22 +1,126 @@
-import { View, Text, StyleSheet} from 'react-native'
-import React from 'react'
+import { Text, ScrollView, StyleSheet, TextInput, TouchableOpacity, Alert,  } from 'react-native'
+import React, { useState } from 'react'
+
 
 export default function Inserir() {
-  return (
-    <View style={css.container}>
-      <Text style={css.text}>Inserir</Text>
-    </View>
-  )
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [genero, setGenero] = useState('');
+    const [error, setError] = useState(false);
+    const [sucesso, setSucesso] = useState(false);
+  
+
+    async function Cadastro()
+    {
+        await fetch("http://10.139.75.34:5251/api/Clients/InsertClients/", {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify( {
+               clientName:nome,
+               clientEmail:email,
+               clientGenere:genero
+            })
+          })
+      
+          .then(( res) => res.json(),setEmail(""),
+          setGenero(""),
+          setNome("") )
+          .then( json => console.log(json))
+          .catch(err => setError( true ) )
+          
+      
+    }
+
+    return (
+        <ScrollView contentContainerStyle={css.container}>
+            {sucesso ?
+                <Text style={css.text}>Obrigado por se cadastrar. Seu cadastro foi realizado com sucesso!!</Text>
+                :
+            <>
+          <Text style={css.text}>Inserir dados:</Text>
+    <TextInput
+    style={css.input}
+    placeholder="Nome "
+     value={nome}
+     onChangeText={(digitado) => setNome( digitado)}
+     />
+     
+     <TextInput
+     style={css.input}
+     placeholder="E-mail"
+     value={email}
+     onChangeText={(digitado) => setEmail( digitado)}
+   />
+
+<TextInput
+     style={css.input}
+     placeholder="Genere"
+     value={genero}
+     onChangeText={(digitado) => setGenero( digitado)}
+   />
+  
+   
+    
+      
+     
+     
+        <TouchableOpacity style={css.btn} onPress={()=> {Cadastro()
+          Alert.alert("Cadastro realizado com sucesso!")
+        }}>
+            <Text style={css.btnText}>Cadastrar</Text>
+        </TouchableOpacity>
+        {error && <Text style={css.errorText}>Revise os campos. Tente Novamente </Text>}
+        </>
+        }
+        </ScrollView>
+    )
 }
+
 const css = StyleSheet.create({
-  container: {
-    backgroundColor: "#191919",
-    flexGrow: 1,
-    color: "white",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  text: {
-    color: "white"
-  }
+    container: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: "#E4E4E5"
+
+
+    },
+    input: {
+        width: "90%",
+        backgroundColor: "white",
+        height: 60,
+        borderRadius: 3,
+        padding: 15,
+        marginTop: 15,
+        marginBottom: 10,
+        borderWidth: 0,
+        elevation: 60,
+
+    },
+    text: {
+        fontSize: 24,
+        marginBottom: 20,
+        color: "black",
+        marginTop: 10
+    },
+    btn: {
+        width: "90%",
+        height: 45,
+        borderRadius: 5,
+        backgroundColor: "#0A2351",
+        marginTop: 10,
+        
+       
+    },
+    btnText: {
+        lineHeight: 45,
+        color: "white",
+        textAlign: "center",
+        fontSize: 20,
+        elevation:90
+    },
+   
 })
